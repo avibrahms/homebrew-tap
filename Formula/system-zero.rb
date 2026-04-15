81564162c30208559c79f75423ece660a832c27d
Avi Pilcer <avibrahms@gmail.com>
2026-04-15 15:10:55 -0300

    use sz-cli pypi package

--- Formula/system-zero.rb
+++ Formula/system-zero.rb
@@ -1,15 +1,15 @@
 class SystemZero < Formula
   desc "One-click autonomy and self-improvement for any repository"
   homepage "https://systemzero.dev"
-  url "https://files.pythonhosted.org/packages/source/s/system-zero-cli/system_zero_cli-0.1.0.tar.gz"
-  sha256 "f03e6d380a63dae6ad4fb06fa142e4da63adcc39865ee3d3632c29592f52d4a2"
+  url "https://files.pythonhosted.org/packages/source/s/sz-cli/sz_cli-0.1.0.tar.gz"
+  sha256 "e829d683b422c3b0113e98baa91fb81483a2c13eb3ca67b5a4f543ab77e1122b"
   license "Apache-2.0"
   depends_on "pipx"
 
   def install
     (bin/"sz").write <<~EOS
       #!/usr/bin/env bash
-      exec "#{Formula["pipx"].opt_bin}/pipx" run --spec system-zero-cli==0.1.0 sz "$@"
+      exec "#{Formula["pipx"].opt_bin}/pipx" run --spec sz-cli==0.1.0 sz "$@"
     EOS
   end
 
