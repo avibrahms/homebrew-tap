64dd703b7f84168c67023b20dedbe5b4f46141e5
Avi Pilcer <avibrahms@gmail.com>
2026-04-15 15:04:16 -0300

    use launchable pypi package

--- Formula/system-zero.rb
+++ Formula/system-zero.rb
@@ -1,15 +1,15 @@
 class SystemZero < Formula
   desc "One-click autonomy and self-improvement for any repository"
   homepage "https://systemzero.dev"
-  url "https://files.pythonhosted.org/packages/source/s/system-zero/system_zero-0.1.0.tar.gz"
-  sha256 "6cf1be1a819a9ff622b9c54dc53b67d3dee94d21480be3cf0acffb58f51269ae"
+  url "https://files.pythonhosted.org/packages/source/s/system-zero-cli/system_zero_cli-0.1.0.tar.gz"
+  sha256 "f03e6d380a63dae6ad4fb06fa142e4da63adcc39865ee3d3632c29592f52d4a2"
   license "Apache-2.0"
   depends_on "pipx"
 
   def install
     (bin/"sz").write <<~EOS
       #!/usr/bin/env bash
-      exec "#{Formula["pipx"].opt_bin}/pipx" run --spec system-zero==0.1.0 sz "$@"
+      exec "#{Formula["pipx"].opt_bin}/pipx" run --spec system-zero-cli==0.1.0 sz "$@"
     EOS
   end
 
