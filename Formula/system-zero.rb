255fbcf3faa294b37cc1c651b240fda1d6b7668d
Avi Pilcer <avibrahms@gmail.com>
2026-04-15 14:56:13 -0300

    install via pipx wrapper

--- Formula/system-zero.rb
+++ Formula/system-zero.rb
@@ -4,13 +4,13 @@ class SystemZero < Formula
   url "https://files.pythonhosted.org/packages/source/s/system-zero/system_zero-0.1.0.tar.gz"
   sha256 "6cf1be1a819a9ff622b9c54dc53b67d3dee94d21480be3cf0acffb58f51269ae"
   license "Apache-2.0"
-  depends_on "python@3.12"
+  depends_on "pipx"
 
   def install
-    system Formula["python@3.12"].opt_libexec/"bin/python3", "-m", "venv", libexec
-    system libexec/"bin/python3", "-m", "pip", "install", "--upgrade", "pip"
-    system libexec/"bin/python3", "-m", "pip", "install", "system-zero==0.1.0"
-    bin.install_symlink libexec/"bin/sz"
+    (bin/"sz").write <<~EOS
+      #!/usr/bin/env bash
+      exec "#{Formula["pipx"].opt_bin}/pipx" run --spec system-zero==0.1.0 sz "$@"
+    EOS
   end
 
   test do
